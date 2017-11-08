8d657f2201786584abf581e439d0abfab66e7db2
Arvidp96 <arvper-5@student.ltu.se>
2017-11-08 17:03:31 +0100

    ListProducts test
    ListProducts test

--- php/ListProducts.php
+++ php/ListProducts.php
@@ -3,9 +3,28 @@
 <head>
     <link rel="stylesheet" type="text/css" href="">
     <meta charset="UTF-8">
-    <title>Title</title>
 </head>
 <body>
+
+<table>
+    <tr>
+    <th>Name</th>
+    <th>Price</th>
+    <th>Amount</th>
+    </tr>
+<?php
+Function ListProducts ($fetchedData){
+foreach ($fetchedData as $row){
+?>
+    <tr>
+    <td><?php echo $row['Name'] . "<br />";?> </td>
+    <td><?php echo $row['Price'] . "<br />";?> </td>
+    <td><?php echo $row['Amount'] . "<br />";?> </td>
+    </tr>
+<?php } ?>
+<?php } ?>
+
+</table>
 
 </body>
 </html>
