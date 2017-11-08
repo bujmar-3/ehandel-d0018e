282186435c73fa88d49466d8de6890ab725882fe
bujmar-3 <marz@hotmail.se>
2017-11-08 18:38:57 +0100

    Nu funkar CSS
    kolla kolla

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/productList.css">
     <title>Test Site</title>
+    <?php include 'php/DbConnection.php'; ?>
+    <?php include 'php/test.php'; ?>
 </head>
 <body>
-    <?php include 'php/DbConnection.php'; ?>
-    <?php include 'php/ListProducts.php'; ?>
     <?php
     ListProducts(getProducts());
     ?>
---
+++ php/test.php
@@ -0,0 +1,13 @@
+<?php
+Function ListProducts ($fetchedData){
+    echo '<table id="productList">';
+    echo '<tr>';
+    echo '<th>Name</th>';
+    echo '<th>Pris</th>';
+    echo '<th>Antal</th>';
+    echo '</tr>';
+    echo '</table>';
+    echo '</body>';
+    echo '</html>';
+}
+?>
