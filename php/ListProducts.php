fbe19588292e6fc85419c572267a0bffda382f28
bujmar-3 <marz@hotmail.se>
2017-11-08 17:43:30 +0100

    push
    push

--- php/ListProducts.php
+++ php/ListProducts.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="../CSS/productList.css">
+    <link rel="stylesheet" type="text/css" href="../css/productList.css">
     <meta charset="UTF-8">
 </head>
 <body>
 
 <table id="productList" border="1px solid black">
     <tr>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Amount</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Amount</th>
     </tr>
 <?php
+
 Function ListProducts ($fetchedData){
 foreach ($fetchedData as $row){
 ?>
     <tr>
-    <td><?php echo $row['Name']?> </td>
-    <td><?php echo $row['Price']?> </td>
-    <td><?php echo $row['Amount']?> </td>
+        <td><?php echo $row['Name']?> </td>
+        <td><?php echo $row['Price']?> </td>
+        <td><?php echo $row['Amount']?> </td>
         </br>
     </tr>
 <?php } ?>
