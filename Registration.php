c5a204b01746868743c3deeca3c6c552a595314f
Arvidp96 <arvper-5@student.ltu.se>
2017-11-22 18:58:43 +0100

    Registration.php
    Registreringssida klar

--- Registration.php
+++ Registration.php
@@ -7,86 +7,39 @@ echo    '<link rel="stylesheet" type="text/css" href="css/productList.css">';
         include 'php/DbConnection.php';
 echo    '</head>';
 echo    '<body>';
-        if (isset($_POST["username"])) {
-            $user = $_POST["username"];
-        }
-        else {
-            $name = null;
-        }
-        if (isset($_POST["name"])) {
-            $name = $_POST["name"];
-        }
-        else {
-            $name = null;
-        }
-        if (isset($_POST["lastname"])) {
-            $lname = $_POST["lastname"];
-        }
-        else {
-            $lname = null;
-        }
-        if (isset($_POST["pass"])) {
-            $pass = $_POST["pass"];
-        }
-        else {
-            $name = null;
-        }
-        if (isset($_POST["pass_rep"])) {
-            $pass_rep = $_POST["pass_rep"];
-        }
-        else {
-            $name = null;
-        }
-        if (isset($_POST["adress"])) {
-            $adress = $_POST["adress"];
-        }
-        else {
-            $adress = null;
-        }
-        if (isset($_POST["zip"])) {
-            $zip = $_POST["zip"];
-        }
-        else {
-            $zip = null;
-        }
-
 ?>
+
     <form method="post" id="register" action="Registration.php">
         <fieldset>
         <legend><b>  Registrering  </b></legend>
         <label for="1"><b>Användarnamn: *</b></label><br><input value="<?php if (isset($_POST["username"])) {echo $_POST["username"];} ?>" type="text" name="username" id="1" required><br><br>
         <label for="2"><b>Namn: </b></label><br><input value="<?php if (isset($_POST["name"])) {echo $_POST["name"];} ?>" type="text" name="name" id="2"><br><br>
         <label for="3"><b>Eftermanm: </b></label><br><input value="<?php if (isset($_POST["lastname"])) {echo $_POST["lastname"];} ?>" type="text" name="lastname" id="3"><br><br>
-        <label for="4"><b>Lösenord: *</b></label><br><input type="text" name="pass" id="4" required><br><br>
-        <label for="5"><b>Upprepa lösenord: *</b><br></label><input type="text" name="pass_rep" id="5" required><br><br>
+        <label for="4"><b>Lösenord: *</b></label><br><input type="password" name="pass" id="4" required><br><br>
+        <label for="5"><b>Upprepa lösenord: *</b><br></label><input type="password" name="pass_rep" id="5" required><br><br>
         <label for="6"><b>Adress: </b></label><br><input value="<?php if (isset($_POST["adress"])) {echo $_POST["adress"];} ?>" type="text" name="adress" id="6"><br><br>
         <label for="7"><b>Postkod: </b></label><br><input value="<?php if (isset($_POST["zip"])) {echo $_POST["zip"];} ?>" type="text" name="zip" id="7"><br><br>
         <input type="submit" name="Registrera">
-        <button name="Button" onclick="">Avbryt</button>
+        <a href="index.php">Gå tillbaka</a>
+        <!-- <button name="Button" onclick="index.php">Avbryt</button> -->
 
-        <?php
+<?php
         check();
         function check() {
             if(isset($_POST['username']))
             {
-                //signUp();
                 $username = $_POST["username"];
-                $name = $_POST["name"];
-                $lname = $_POST["lastname"];
                 $pass = $_POST["pass"];
                 $pass_rep = $_POST["pass_rep"];
-                $adress = $_POST["adress"];
-                $zip = $_POST["zip"];
-                signUp($username, $name, $lname, $pass, $pass_rep, $adress, $zip);
+                signUp($username, $pass, $pass_rep);
             }
         }
 
+echo '</fieldset>
+     </form>';
+    
 
-
-echo '</fieldset>';
-echo '</form>';
-
-Function signUp($user, $name, $lname, $pass, $pass_rep, $adress, $zip)
+Function signUp($user, $pass, $pass_rep)
 {
 
         if ($pass_rep==$pass) {
@@ -95,7 +48,7 @@ Function signUp($user, $name, $lname, $pass, $pass_rep, $adress, $zip)
             $prepState->execute();
             $fetchedData = $prepState->fetchAll();
             if (count($fetchedData) == 0){
-                newUser($user, $name, $lname, $pass, $adress, $zip);
+                newUser();
                 return true;
             }
             else {
@@ -107,12 +60,47 @@ Function signUp($user, $name, $lname, $pass, $pass_rep, $adress, $zip)
         }
 }
 
-Function newUser($username, $name, $lname, $pass, $adress, $zip)
+Function newUser()
 {
-
+    if (isset($_POST["username"])) {
+        $user = $_POST["username"];
+    }
+    else {
+        $user = null;
+    }
+    if (isset($_POST["name"])) {
+        $name = $_POST["name"];
+    }
+    else {
+        $name = null;
+    }
+    if (isset($_POST["lastname"])) {
+        $lname = $_POST["lastname"];
+    }
+    else {
+        $lname = null;
+    }
+    if (isset($_POST["pass"])) {
+        $pass = $_POST["pass"];
+    }
+    else {
+        $pass = null;
+    }
+    if (isset($_POST["adress"])) {
+        $adress = $_POST["adress"];
+    }
+    else {
+        $adress = null;
+    }
+    if (isset($_POST["zip"])) {
+        $zip = $_POST["zip"];
+    }
+    else {
+        $zip = null;
+    }
     $conn = connectDb();
-    $sql = "INSERT INTO `users` (`UserID`, `UserName`, `Fname`, `Lname`, `Password`, `Adress`, `Zipcode`, `UserType`) VALUES (DEFAULT ,$username, $name, $lname, $pass, $adress, $zip, '1')";
-    if ($conn->query($sql) === TRUE) {
+    $sql = "INSERT INTO `users` (`UserID`,`UserName`, `Fname`, `Lname`, `Password`, `Adress`, `Zipcode`, `UserType`) VALUES (DEFAULT ,'$user', '$name', '$lname', '$pass', '$adress', '$zip', '1')";
+    if ($conn->exec($sql) == TRUE) {
         echo '<script> alert("Ny användare skapad!") </script>';
     } else {
         echo "Error";
