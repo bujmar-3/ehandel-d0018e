2664537aeb9a3497177f87adbcd0c117fbd8fcf3
bujmar-3 <marz@hotmail.se>
2017-12-02 17:19:19 +0100

    Cart - Visa inehåll - lista kundvagnar
    Nu listas kundvagnar och du kan se dess innehåll

--- php/ShoppingCart.php
+++ php/ShoppingCart.php
@@ -5,6 +5,8 @@
  * Date: 2017-12-01
  * Time: 16:09
  */
+
+/**Listar och visar alla kundvagnar som personen har*/
 function listCarts(){
     if(checkLoggedIn())
     {
@@ -38,6 +40,7 @@ function listCarts(){
     }
 }
 
+/**Hämtar alla kundvagnar och dess data från databasen*/
 function getCartData(){
     $userid = $_SESSION["userid"];
     $conn = connectDb();
@@ -47,11 +50,12 @@ function getCartData(){
     return $fetchedData;
 }
 
+/**Skapar en tabell av personliga kundvagnar*/
 function createCartTable($cartList){
     foreach ($cartList as $row){
         echo '
             <tr>
-                <td>'. $row['Name'] .'</td>
+                <td><a href="Cart.php?ID=' . $row['InstanceID'] . '&Name='. $row['Name'] .'">' . $row['Name'] . '</a></td>
                 <td>'. $row['Date'] .'</td>
                 <td>'. $row['Status'] .'</td>
             </tr>
@@ -59,6 +63,7 @@ function createCartTable($cartList){
     }
 }
 
+/**Skapar en rad innehållande knapp för att skapa ny kundvagn*/
 function createNewCartButton(){
     $todayDate = date('Y-m-d');
     echo'
@@ -72,12 +77,17 @@ function createNewCartButton(){
     ';
 }
 
+/**Kollar om GET eller POST anrop mot sidan körts*/
 function checkCartPost(){
     if(isset($_POST["newCartName"])){
         createNewCart();
     }
+    if(isset($_GET['ID'])){
+        showCart($_GET['ID'],$_GET['Name']);
+    }
 }
 
+/**Skapar ny kundvagn med data från POST*/
 function createNewCart(){
     $todayDate = date('Y-m-d');
     $name = $_POST["newCartName"];
@@ -86,3 +96,45 @@ function createNewCart(){
     $prepState = $conn->prepare("INSERT INTO order_instance(InstanceID, Date, UserID, Name, Status) VALUES (DEFAULT, '$todayDate', $userID, '$name', 1)");
     $prepState->execute();
 }
+
+/**Visar innehåll av kundvagn baserat på kundvagnsID*/
+function showCart($cartId, $cartName)
+{
+    echo '
+          <table id="productList">
+            <tr>
+                <th>' . $cartName . '</th>
+                <th></th>
+                <th></th>
+            </tr>
+    ';
+    $cartProductList = getCartProducts($cartId);
+    foreach ($cartProductList as $row) {
+        echo '
+            <tr>
+                <td>' . $row['Name'] . '</td>
+                <td>' . $row['Amount'] . 'st</td>
+                <td>' . $row['Price'] . 'kr</td>
+            </tr>
+        ';
+    }
+    echo'
+            <form id="checkOut" action="Cart.php" method="post">
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><input type="submit" value="Checka ut"></td>
+                </tr>
+            </form>
+        </table>
+    ';
+}
+
+    function getCartProducts($cartID)
+    {
+        $conn = connectDb();
+        $prepState = $conn->prepare("SELECT product.Name, orders.Amount, orders.Price FROM product, orders WHERE orders.instanceID = $cartID && orders.ProductID = product.ProductID");
+        $prepState->execute();
+        $fetchedData = $prepState->fetchAll();
+        return $fetchedData;
+    }
