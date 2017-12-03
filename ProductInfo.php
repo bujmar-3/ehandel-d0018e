eb9a563738ffc5de21341d154c252f76f9d066b8
Arvidp96 <arvper-5@student.ltu.se>
2017-12-03 17:24:22 +0100

    ProductInfo + css

--- ProductInfo.php
+++ ProductInfo.php
@@ -1,8 +1,10 @@
-
 <!DOCTYPE html>
-<html lang="sv">
+<html lang="sv" xmlns="http://www.w3.org/1999/html">
 <head>
+    <link rel="stylesheet" type="text/css" href="css/productList.css">
     <meta charset="UTF-8">
+    <?php include 'php/Navbar.php'; ?>
+    <?php include 'php/LoginForm.php'; ?>
     <?php include 'php/DbConnection.php'; ?>
 </head>
 <body>
@@ -31,26 +33,53 @@ else {
     exit();
 }
 ?>
-<table border="2px solid black">
-    <tr>
-        <td><?php echo "$namn" ?></td>
-    </tr>
-    <tr>
-        <td>Pris:     </td>
-        <td><?php echo "$pris" ?></td>
-    </tr>
-    <tr>
-        <td>Antal:     </td>
-        <td><?php echo "$antal" ?></td>
-    </tr>
-    <tr>
-        <td>Beskrivning:     </td>
-        <td><?php echo "$beskrivning" ?></td>
-    </tr>
-    <tr>
-        <td>Snittbetyg:     </td>
-    </tr>
+<div id="header">
+    <div id="navmenu">
+        <?php
+        getNavBar();
+        ?>
+    </div>
+</div>
+<div id="wrapper">
+    <div id="content">
+        <table id="productList">
+            <tr>
+                <th colspan="2"><?php echo "$namn" ?></th>
+            </tr>
+            <tr>
+                <td>Pris:     </td>
+                <td><?php echo "$pris" ?></td>
+            </tr>
+            <tr>
+                <td>Antal:     </td>
+                <td><?php echo "$antal" ?></td>
+            </tr>
+            <tr>
+                <td>Beskrivning:     </td>
+                <td><?php echo "$beskrivning" ?></td>
+            </tr>
+            <tr>
+                <td>Snittbetyg:     </td>
+            </tr>
 
-</table>
+        </table>
+        <br><br>
+        <button id="addToCartButton">Lägg till i kundvagn</button>
+        <br><br><br><br>
+        <h2>Skriv kommentar:</h2>
+        <form id="addComment" method="post" action="index.php">
+            <label><textarea rows="6" cols="124" form="addComment">Skriv här...</textarea></label>
+            <input type="submit" name="Kommentera" value="Kommentera">
+        </form>
+    </div>
+    <div id="right-sidebar">
+        <?php
+        getLoginForm();
+        ?>
+    </div>
+</div>
+
+
+
 </body>
 </html>
