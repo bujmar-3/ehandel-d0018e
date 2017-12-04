286972f400c62149e515119cf5a44a9ffdc5f871
bujmar-3 <marz@hotmail.se>
2017-12-04 16:42:37 +0100

    Checkout - påbörjar checkout

---
+++ php/Checkout.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/productList.css">
+    <title>Test Site</title>
+    <?php include 'php/DbConnection.php'; ?>
+    <?php include 'php/ShoppingCart.php'; ?>
+    <?php include 'php/LoginForm.php'; ?>
+    <?php include 'php/Navbar.php'; ?>
+</head>
+<body>
+<?php session_start(); ?>
+<div id="header">
+    <div id="navmenu">
+        <?php
+        getNavBar();
+        ?>
+    </div>
+</div>
+<div id="wrapper">
+    <div id="content">
+
+    </div>
+    <div id="right-sidebar">
+        <?php
+        getLoginForm();
+        ?>
+    </div>
+</div>
+</body>
+</html>
---
+++ php/CheckoutForm.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MarZ
+ * Date: 2017-12-04
+ * Time: 16:42
+ */
