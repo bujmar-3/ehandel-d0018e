83d820ddc19d3d38ddc7d84fb7f5348768485c67
Arvidp96 <arvper-5@student.ltu.se>
2017-12-05 16:28:24 +0100

    fil fix

--- Product.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: MarZ
- * Date: 2017-11-30
- * Time: 17:55
- */
---
+++ Profile.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arvid
+ * Date: 2017-12-05
+ * Time: 16:27
+ */
