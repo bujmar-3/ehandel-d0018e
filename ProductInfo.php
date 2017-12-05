271e42584c7ad7dca075641988150d9850c1635d
Arvidp96 <arvper-5@student.ltu.se>
2017-12-05 16:25:07 +0100

    ProductInfo upp

--- ProductInfo.php
+++ ProductInfo.php
@@ -10,6 +10,8 @@
 <body>
 <?php
     session_start();
+
+    //Hämta information om produkten
     if (isset($_GET['ID'])) {
         $id = $_GET['ID'];
         $conn = connectDb();
@@ -33,29 +35,39 @@
         echo "Ingen produkt i systemet har detta ID";
         exit();
     }
+
+    //Lägg till i kundvagn
     if (isset($_POST['addToCartButton'])) {
-        addToCart($id, $pris, 1);
-        echo '<script> alert("Produkten är nu tillagd i din kundvagn")</script>';
-    }
-    // Skriv kommentar
-    if (isset($_POST['comment']) && (isset($_POST['productGrade']))) {
-        $conn = connectDb();
-        $com = $_POST['comment'];
-        $grade = $_POST['productGrade'];
-        $user = $_SESSION['userid'];
-        $sql = "INSERT INTO `rating` (`RatingID`,`ProductID`,`Comment`,`Rating`,`UserID`) VALUES (DEFAULT ,'$id', '$com', '$grade', '$user')";
-        if ($conn->exec($sql) == TRUE) {
-            echo '<script> alert("Kommentar tillagd!") </script>';
+        if (checkLoggedIn() == TRUE) {
+            if ( isset($_SESSION['activecart']) && $_SESSION['activecart'] == !NULL) {
+                addToCart($id, $pris, 1);
+                echo '<script> alert("Produkten är nu tillagd i din kundvagn")</script>';
+            }
+            else {
+                echo '<script> alert("Du måste välja en kundvagn att lägga produkten i!")</script>'; 
+            }
         }
         else {
-            echo '<script> alert("Error: Kan inte lägga till data i databasen") </script>';
+            echo '<script> alert("Du måste vara inloggad för att kunna lägga till produkter i kundvagnen!")</script>';
         }
     }
+
+    // Skriv kommentar
+    if (isset($_POST['comment']) && (isset($_POST['productGrade']))) {
+        if (checkLoggedIn() == TRUE) {
+            writeComment();
+        }
+        else {
+            echo '<script> alert("Gör inte en arvid, logga in först!") </script>';
+        }
+    }
+
 ?>
+
 <div id="header">
     <div id="navmenu">
         <?php
-        getNavBar();
+            getNavBar();
         ?>
     </div>
 </div>
@@ -79,6 +91,7 @@
             </tr>
             <tr>
                 <td>Snittbetyg:     </td>
+                <td><?php $snitt  = getGrade(); echo $snitt;?></td>
             </tr>
 
         </table>
@@ -86,14 +99,61 @@
         <form action="ProductInfo.php?ID=<?php echo $id; ?>" method="post">
             <input id="addToCartButton" type="submit" value="Lägg till i kundvagn" name="addToCartButton">
         </form>
-        <br><br>
+        <br><br><br><br>
+        <h2>Recensioner:</h2>
         <?php
-        $prepState = $conn->prepare("SELECT users.UserName, rating.Comment, rating.Rating, rating.RatingID FROM rating,users WHERE rating.ProductID = $id && users.UserID = rating.UserID");
-        $prepState->execute();
-        $fetchedData = $prepState->fetchAll();
+            getComments();
+        ?>
+        <br><br><br><br>
+        <h2>Skriv recension:</h2>
+        <form id="addComment" method="post" action="ProductInfo.php?ID=<?php echo $id; ?>">
+            <label><textarea rows="6" cols="124" form="addComment" name="comment" required>Skriv här...</textarea></label><br><br>
+             <label>Ditt betyg:  <input type="number" name="productGrade" min="1" max="5" required></label> <br><br>
+            <input type="submit" name="recension" value="Recensera">
+        </form>
+        <br><br><br><br><br><br>
+    </div>
+    <div id="right-sidebar">
+        <?php
+            getLoginForm();
+        ?>
+    </div>
+</div>
+
+</body>
+</html>
+<?php
+
+function getGrade () {
+    $conn = connectDb();
+    $id = $_GET['ID'];
+    $count = 0;
+    $num = 0;
+    $sql = $conn->prepare("SELECT rating.Rating FROM rating,users WHERE rating.ProductID = $id");
+    $sql->execute();
+    $check = $sql->fetchAll();
+    if (count($check) > 0){
+        foreach ($check as $row) {
+            $num = $num + $row['Rating'];
+            $count = $count+1;
+        }
+        return ($num/$count . " / 5 ");
+    }
+    else {
+        return ("Det finns inga betyg");
+    }
+}
+
+function getComments () {
+    $conn = connectDb();
+    $id = $_GET['ID'];
+    $prepState = $conn->prepare("SELECT users.UserName, rating.Comment, rating.Rating, rating.RatingID FROM rating,users WHERE rating.ProductID = $id && users.UserID = rating.UserID");
+    $prepState->execute();
+    $fetchedData = $prepState->fetchAll();
+    if (count($fetchedData) > 0){
         foreach ($fetchedData as $row)
         {
-            echo "<br><br><br>";
+            echo "<br><br>";
             echo '<b>' . $row['UserName'] . '</b>';
             echo "<br><br>";
             echo $row['Comment'];
@@ -102,25 +162,28 @@
             echo "<br><br>";
             echo "_________________________________________________________________________________________________________________";
         }
-        ?>
-        <br><br><br><br>
-        <h2>Skriv recension:</h2>
-        <form id="addComment" method="post" action="ProductInfo.php?ID=<?php echo $id; ?>">
-            <label><textarea rows="6" cols="124" form="addComment" name="comment" required>Skriv här...</textarea></label><br><br>
-             <label>Ditt betyg:  <input type="number" name="productGrade" required></label> <br><br>
-            <input type="submit" name="recension" value="Recensera">
-        </form>
-    </div>
-    <div id="right-sidebar">
-        <?php
-        getLoginForm();
-        ?>
-    </div>
-</div>
+    }
+    else {
+        echo '<br>';
+        echo "Det finns inga recensioner.";
+    }
+}
 
-</body>
-</html>
-<?php
+function writeComment () {
+    $conn = connectDb();
+    $id = $_GET['ID'];
+    $com = $_POST['comment'];
+    $grade = $_POST['productGrade'];
+    $user = $_SESSION['userid'];
+    $sql = "INSERT INTO rating (RatingID,ProductID,Comment,Rating,UserID) VALUES (DEFAULT ,$id, '$com', $grade, $user)";
+    if ($conn->exec($sql) == TRUE) {
+        echo '<script> alert("Kommentar tillagd!") </script>';
+    }
+    else {
+        echo '<script> alert("Error: Kan inte lägga till data i databasen") </script>';
+    }
+}
+
 function addToCart($productID, $price, $amount){
     $cartID =$_SESSION['activecart'];
     $conn = connectDb();
