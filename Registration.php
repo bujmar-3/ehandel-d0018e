04b12c2c7e54f0b9b4e640117587654e7aad513d
Arvidp96 <arvper-5@student.ltu.se>
2017-12-13 14:52:30 +0100

    final update + css fix

--- Registration.php
+++ Registration.php
@@ -2,7 +2,7 @@
 <html lang="sv">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="css/productList.css">
+    <link rel="stylesheet" type="text/css" href="css/Layout.css">
     <?php include 'php/DbConnection.php'; ?>
     <?php include 'php/LoginForm.php'; ?>
     <?php include 'php/Navbar.php'; ?>
@@ -43,11 +43,11 @@ echo '
 
 function registrationForm() {
     echo      '<br><form method="post" id="register" action="Registration.php">';
-    echo            '<label for="1"><b>Användarnamn: *</b></label><br><input value="'; getPreset("username"); echo  '" type="text" name="username" id="1" required><br><br>';
+    echo            '<label for="1"><b>Användarnamn: *</b></label><br><input value="'; getPreset("username"); echo  '" type="text" name="username" id="r1" required><br><br>';
     echo            '<label for="2"><b>Namn: </b></label><br><input value="'; getPreset("name"); echo '" type="text" name="name" id="2"><br><br>';
     echo            '<label for="3"><b>Eftermanm: </b></label><br><input value="'; getPreset("lastname"); echo '" type="text" name="lastname" id="3"><br><br>';
     echo            '<label for="4"><b>Lösenord: *</b></label><br><input value="" type="password" name="pass" id="regPass" required><br><br>
-                    <label for="5"><b>Upprepa lösenord: *</b><br></label><input value="" type="password" name="pass_rep" id="regPass_rep" required><br><br>';
+                    <label for="5"><b>Upprepa lösenord: *</b><br></label><input value="" type="password" name="pass_rep" id="regPass" required><br><br>';
     echo            '<label for="6"><b>Adress: </b></label><br><input value="'; getPreset("adress"); echo '" type="text" name="adress" id="6"><br><br>';
     echo            '<label for="7"><b>Postkod: </b></label><br><input value="'; getPreset("zip"); echo '" type="number" name="zip" id="7"><br><br> 
                     <input type="submit" name="Registrera" value="Registrera">
